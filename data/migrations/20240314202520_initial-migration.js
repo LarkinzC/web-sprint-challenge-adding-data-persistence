44c1b1b2a663c49a49aaaea405c75dd271186c30
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-14 13:46:14 -0700

    tables building

--- data/migrations/20240314202520_initial-migration.js
+++ data/migrations/20240314202520_initial-migration.js
@@ -6,12 +6,27 @@ exports.up = async function(knex) {
     await knex.schema
     .createTable('projects', table => {
         table.increments('project_id')
+        table.string('project_name').notNullable()
+        table.string('project_description')
+        table.boolean('project_completed').defaultTo(0)
     })
     .createTable('resources', table => {
         table.increments('resource_id')
+        table.string('resource_name').notNullable().unique()
+        table.string('resource_description')
     })
     .createTable('tasks', table => {
         table.increments('task_id')
+        table.string('task_description').notNullable()
+        table.string('task_notes')
+        table.boolean('task_completed').defaultTo(0)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
     .createTable('project_resources', table => {
         table.increments('pro_res_id')
